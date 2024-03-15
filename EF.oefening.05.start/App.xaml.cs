2cc2ba206d78f76ac01351a53cc2cc5153bf4f38
RonnySweetlove <RonnySweetlove@E40ITP-PF32EFX8>
2024-03-15 22:58:04 +0100

    Add project files.

---
+++ EF.oefening.05.start/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EF.oefening._05.start
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ EF.oefening.05.start/Dialoog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EF.oefening._05.start
+{
+    /// <summary>
+    /// Interaction logic for Dialoog.xaml
+    /// </summary>
+    public partial class Dialoog : Window
+    {
+        public Dialoog()
+        {
+            InitializeComponent();
+        }
+    }
+}
